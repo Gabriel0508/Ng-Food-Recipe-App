cc2c62ff597ea03ff8b0beb04b580ab367d779f5
alexPopNicolae <alexpop3u@gmail.com>
2023-02-14 14:42:52 +0200

    RCP#1: created basic page components for app pages

--- src/app/pages/instructions/instructions.module.ts
+++ src/app/pages/instructions/instructions.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InstructionsComponent } from './instructions/instructions.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InstructionsComponent
+  ],
   imports: [
     CommonModule
   ]
--- src/app/pages/recipes/recipes.module.ts
+++ src/app/pages/recipes/recipes.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RecipesComponent } from './recipes/recipes.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RecipesComponent
+  ],
   imports: [
     CommonModule
   ]
