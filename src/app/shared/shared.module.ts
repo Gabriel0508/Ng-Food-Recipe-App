29b61117c5fd5c262c23906502dc08151b19cd3e
alexPopNicolae <alexpop3u@gmail.com>
2023-02-20 09:08:03 +0200

    RCP#6 added title and background for the blog area

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { ComponentsModule } from '../components/components.module';
 import { HeroAreaComponent } from './hero-area/hero-area.component';
 import { TrendingComponent } from './trending/trending.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
-  declarations: [HeaderComponent, HeroAreaComponent, TrendingComponent],
+  declarations: [HeaderComponent, HeroAreaComponent, TrendingComponent, BlogComponent],
   imports: [CommonModule, ComponentsModule],
-  exports: [HeaderComponent,HeroAreaComponent,TrendingComponent],
+  exports: [HeaderComponent,HeroAreaComponent,TrendingComponent, BlogComponent],
 })
 export class SharedModule {}
