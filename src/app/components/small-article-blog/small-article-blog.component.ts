5adcd399a500c3dc590ce60bcf8f3e1a453d4e26
Alex Pop <alexpop3u@gmail.com>
2023-02-21 20:49:43 +0200

    RCP#6: removed useless console.log

--- src/app/components/small-article-blog/small-article-blog.component.ts
+++ src/app/components/small-article-blog/small-article-blog.component.ts
@@ -1,19 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-article-blog',
   templateUrl: './small-article-blog.component.html',
   styleUrls: ['./small-article-blog.component.scss']
 })
-export class SmallArticleBlogComponent implements OnInit {
+export class SmallArticleBlogComponent {
 
   @Input() imageLink: string = "../../../assets/background/smallarticle1.png";
   @Input() date: string = "May 20, 2019";
   @Input() title: string = "Our recipe blog";
   @Input() excerpt: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-
-  ngOnInit(): void {
-    console.log('asta e ceea ce vine pe teava: ', this.imageLink, this.date, this.title, this.excerpt);
-  }
 
 }
